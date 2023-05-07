629990da6f54c0718a93efa91622ed74d220dbbe
Valentin <valio990210@gmail.com>
2023-05-07 17:20:39 +0300

    right and wrnog

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -6,18 +6,21 @@ export function Container() {
 
     const [index, setIndex] = useState(0)
     let data = info[index]
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-    let hasPrev = index > 0 
 
-    function handleNext(){
+
+    let hasPrev = index > 0
+
+    function handleNext() {
         const nextIndex = index + 1;
         setIndex(nextIndex >= info.length ? 0 : nextIndex);
     }
 
-    function handlePrevious(){
-        if(hasPrev){
+    function handlePrevious() {
+        if (hasPrev) {
             setIndex(index - 1)
-        } else{
+        } else {
             setIndex(info.length - 1)
         }
     }
@@ -36,15 +39,29 @@ export function Container() {
                     <button onClick={handlePrevious} className="btnPrev">Previous</button>
                 </div>
             </div>
-
+            
             <div className="answer-section">
                 {Object.keys(data)
                     .filter(key => key.startsWith("answer"))
                     .map(key => (
-                        <button key={key} className="quiz-button">
+                        <button
+                            key={key}
+                            className={`quiz-button${key === selectedAnswer ? (key === 'answerCorrect' ? ' correct' : ' incorrect') : ''}`}
+                            onClick={() => setSelectedAnswer(key)}
+                        >
                             {data[key]}
                         </button>
-                    ))}
+                    ))
+                    .concat(
+                        Object.keys(data).map((key) =>
+                            key === 'answerCorrect' ? (
+                                <button key={key} className={`answer-button correct`}>
+                                    {data[key]}
+                                </button>
+                            ) : null
+                        )
+                    )
+                }
             </div>
 
         </div>
